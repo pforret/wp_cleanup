3c3232122c3a3ae5015d18679720c6f163b3a867
Peter Forret <peter@forret.com>
2020-01-20 23:20:53 +0100

    reinstall some WP folders

--- cleanup_wp.sh
+++ cleanup_wp.sh
@@ -8,3 +8,23 @@ if [[ "$1" == "" ]] ; then
   exit 0
 fi
 
+STARTDIR=$(pwd)
+
+if [[ ! -f "wordpress/wp-config-sample.php" ]] ; then
+	wget https://wordpress.org/latest.zip
+	unzip latest.zip
+fi
+
+overwrite(){
+	nbrsync=$(rsync -rva "$1" "$2" | wc -l)
+	echo "$nbrsync files written to [$2]"
+}
+
+for WP in $(find "$1" -type f -name wp-config.php) ; do
+	WPROOT=$(dirname "$WP")
+	overwrite "wordpress/wp-admin"	"$WPROOT/"
+	overwrite "wordpress/wp-includes"	"$WPROOT/"
+	overwrite "wordpress/wp-content/themes"	"$WPROOT/wp-content/"
+	overwrite "wordpress/wp-content/plugins/akismet"	"$WPROOT/wp-content/plugins/"
+done
+
