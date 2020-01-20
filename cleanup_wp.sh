17555a003d539cf2e5db1bc7a8da4964258e9589
Peter Forret <peter@forret.com>
2020-01-20 23:34:53 +0100

    better output

--- cleanup_wp.sh
+++ cleanup_wp.sh
@@ -11,8 +11,18 @@ fi
 STARTDIR=$(pwd)
 
 if [[ ! -f "wordpress/wp-config-sample.php" ]] ; then
-	wget https://wordpress.org/latest.zip
-	unzip latest.zip
+	echo "First download clean install version of Wordpress"
+	WP_URL="https://wordpress.org/latest.zip"
+	WP_ZIP=$(basename $WP_URL)
+	wget -q $WP_URL
+	if [[ -f $WP_ZIP ]] ; then
+		du -h $WP_ZIP
+		nbunzip=$(unzip $WP_ZIP | wc -l)
+		echo "$nbunzip files in clean install of WP"
+	else
+		echo "Download did not work. Are you sure you have write perlission in this folder [$(pwd)]?"
+		exit 1
+	fi
 fi
 
 overwrite(){
@@ -22,6 +32,7 @@ overwrite(){
 
 for WP in $(find "$1" -type f -name wp-config.php) ; do
 	WPROOT=$(dirname "$WP")
+	echo "## FOLDER $WPROOT"
 	overwrite "wordpress/wp-admin"	"$WPROOT/"
 	overwrite "wordpress/wp-includes"	"$WPROOT/"
 	overwrite "wordpress/wp-content/themes"	"$WPROOT/wp-content/"
